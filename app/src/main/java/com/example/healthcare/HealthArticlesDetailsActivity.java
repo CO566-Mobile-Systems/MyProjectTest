4658314b90af7e70905daac07359a37e416fe41c
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-05-12 20:19:59 +0100

    added click lis

--- app/src/main/java/com/example/healthcare/HealthArticlesDetailsActivity.java
+++ app/src/main/java/com/example/healthcare/HealthArticlesDetailsActivity.java
@@ -2,7 +2,9 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,7 +24,22 @@ public class HealthArticlesDetailsActivity extends AppCompatActivity {
         tv1 = findViewById(R.id.textViewHADTitle);
         img = findViewById(R.id.imageViewHAD);
 
-        
+        Intent intent=getIntent();
+        tv1.setText(intent.getStringExtra("text1"));
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle !=null){
+            int resId = bundle.getInt("text2");
+            img.setImageResource(resId);
+        }
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HealthArticlesDetailsActivity.this, HealthArticlesActivity.class));
+            }
+        });
+
 
     }
 }
