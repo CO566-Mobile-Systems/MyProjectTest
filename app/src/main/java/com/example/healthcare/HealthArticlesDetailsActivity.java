7d090a7386177b831eda9237074b9ea8ed0c83f9
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-05-12 20:09:37 +0100

    update

---
+++ app/src/main/java/com/example/healthcare/HealthArticlesDetailsActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HealthArticlesDetailsActivity extends AppCompatActivity {
+
+    TextView tv1;
+    ImageView img;
+
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_articles_details);
+
+        btnBack = findViewById(R.id.buttonHADBack);
+        tv1 = findViewById(R.id.textViewHADTitle);
+        img = findViewById(R.id.imageViewHAD);
+
+        
+
+    }
+}
