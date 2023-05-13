2036d2c262e5993dd7d07904d63f4f6b14791468
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-05-13 10:30:57 +0100

    Update HomeActivity.java

--- app/src/main/java/com/example/healthcare/HomeActivity.java
+++ app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -54,7 +54,6 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this,AboutUsActivity.class));
             }
         });
-
         CardView contact  = findViewById(R.id.cardContactUs);
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
